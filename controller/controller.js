7ac0c738b98fd76b2a3eb134262bacf5bf9ac691
Vladislav <vla97657234@gmail.com>
2019-03-02 11:08:26 +0800

    Контроллер готов к запуску

--- controller/controller.js
+++ controller/controller.js
@@ -14,14 +14,19 @@ class Controller{
     Вызываем метод объекта рождения
     Присваиваем новое поле полю конструктора, возвращаем в ГУИ
     Вызываем смерть клетки, возвращаем в ГУИ
+    Дергаем глобальный массив и метод для отрисовки
      */
     gameLoop(object){
 	object.born();
 	this.mainField = object.getField();
+	subFieldArr = this.mainField;
+	drawSubField();
 	//to Client
 	//pause
 	object.death();
 	this.mainField = object.getField();
+	subFieldArr = this.MainField;
+	drawSubField();
 	//to Client
 	//pause
     }
